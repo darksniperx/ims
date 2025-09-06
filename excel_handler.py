b2eca5f1225c7b12cf541537f0d0e461f267540d
darksniperx <darksniperx945@gmail.com>
2025-09-06 13:23:34 +0530

    Create excel_handler.py

---
+++ excel_handler.py
@@ -0,0 +1,20 @@
+# bot/excel_handler.py
+import pandas as pd
+import io
+from .database import fs, db
+
+def load_all_excels():
+    # ... (your load_all_excels function)
+    pass
+
+def save_excel_to_gridfs(file_data, filename):
+    # ... (your save_excel_to_gridfs function)
+    pass
+
+def get_excel_files():
+    # ... (your get_excel_files function)
+    pass
+
+def load_excel_on_startup():
+    # ... (your load_excel_on_startup function)
+    pass
